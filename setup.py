2b00a72c3d99fe0d3a1ea3a5489d3c96b23b6086
Richard Neumann <r.neumann@homeinfo.de>
2023-05-04 11:03:12 +0200

    Remove data_files

--- setup.py
+++ setup.py
@@ -14,12 +14,6 @@ setup(
     python_requires='>=3.8',
     py_modules=['mollyguardctl'],
     entry_points={'console_scripts': ['mollyguardctl = mollyguardctl:main']},
-    data_files=[
-        ('/usr/lib/systemd/system', [
-            'clear-luks-autodecrypt-key.service',
-            'mollyguard.service'
-        ])
-    ],
     url='https://github.com/conqp/',
     license='GPLv3',
     description='Mollyguards your system.'
